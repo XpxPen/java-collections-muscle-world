57e0eabb53ed015be281ddab5e802995286b922a
Martin Burolla <mburolla@xpanxion.com>
2023-02-12 18:12:42 -0500

    update

--- src/main/java/DataStoreReadable.java
+++ src/main/java/DataStoreReadable.java
@@ -1,6 +1,16 @@
 import java.util.ArrayList;
 
 public interface DataStoreReadable {
+    /**
+     * Gets a Person for the id specified.
+     * @param id
+     * @return
+     */
     Person getPerson(Integer id);
+
+    /**
+     * Gets all the people from the datastore in no particular order.
+     * @return
+     */
     ArrayList<Person> getAllPeople();
 }
