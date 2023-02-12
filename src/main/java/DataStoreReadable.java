47ad5af52b12709f35ed7132a360ae5cbee8c783
Martin Burolla <mburolla@xpanxion.com>
2023-02-12 18:17:38 -0500

    update

--- src/main/java/DataStoreReadable.java
+++ src/main/java/DataStoreReadable.java
@@ -3,14 +3,11 @@ import java.util.ArrayList;
 public interface DataStoreReadable {
     /**
      * Gets a Person for the id specified.
-     * @param id
-     * @return
      */
     Person getPerson(Integer id);
 
     /**
      * Gets all the people from the datastore in no particular order.
-     * @return
      */
     ArrayList<Person> getAllPeople();
 }
--- src/main/java/DataStoreWritable.java
+++ src/main/java/DataStoreWritable.java
@@ -3,9 +3,6 @@ public interface DataStoreWritable {
     /**
      * Returns true if a person was successfully added to the datastore, otherwise false.
      * Duplicate user ids are not permitted.
-     * @param id
-     * @param person
-     * @return
      */
     Boolean addPerson(Integer id, Person person);
 }
